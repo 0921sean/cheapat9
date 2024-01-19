5d86aea3d6586e10d00b7435d0c11cc8d1f29c4b
smmin21 <nstern999@gmail.com>
2024-01-19 11:59:21 +0900

    [01.19 수민] Pessimistic Lock 추가

--- src/main/java/project/cheap9/repository/ItemRepository.java
+++ src/main/java/project/cheap9/repository/ItemRepository.java
@@ -1,10 +1,12 @@
 package project.cheap9.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 import project.cheap9.domain.Item;
 
 import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
 import java.util.List;
 
 @Repository
@@ -22,7 +24,7 @@ public class ItemRepository {
     }
 
     public Item findOne(Long id) {
-        return em.find(Item.class, id);
+        return em.find(Item.class, id, LockModeType.PESSIMISTIC_WRITE);
     }
 
     public List<Item> findAll() {
